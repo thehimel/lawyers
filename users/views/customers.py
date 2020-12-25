92e65dda04f81ef3989a46707c84bbc584d63683
thehimel <himeldas66@gmail.com>
2020-12-25 21:30:45 +0100

    CustomerSignupView updated

--- users/views/customers.py
+++ users/views/customers.py
@@ -1,16 +1,16 @@
 from allauth.account.views import SignupView
-from users.forms.employees import EmployeeSignupForm
+from users.forms.customers import CustomerSignupForm
 
 
-# Employee Signup View
-class EmployeeSignupView(SignupView):
+# Customer Signup View
+class CustomerSignupView(SignupView):
 
     template_name = 'users/signup.html'  # Custom template is mandatory
-    form_class = EmployeeSignupForm
+    form_class = CustomerSignupForm
     redirect_field_name = 'next'  # Important to redirect user if has next url
 
     # This is mandatory and copy-pasted
     def get_context_data(self, **kwargs):
-        ret = super(EmployeeSignupView, self).get_context_data(**kwargs)
+        ret = super(CustomerSignupView, self).get_context_data(**kwargs)
         ret.update(self.kwargs)
         return ret
