cbf2b0884ceb88ccc9687c8cf8724eb9e2784ff4
thehimel <himeldas66@gmail.com>
2020-12-25 21:44:13 +0100

    core/urls.py updated

--- core/urls.py
+++ core/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     # url: '/', name = core:home
     path('', HomeView.as_view(), name='home'),
 
-    # url: '/customers/dashboard', name = core:customer_dashboard
-    path('customers/dashboard',
-         CustomerDashboardView.as_view(), name='customer_dashboard'),
-
     # url: '/lawyers/dashboard', name = core:lawyer_dashboard
     path('lawyers/dashboard',
          LawyerDashboardView.as_view(), name='lawyer_dashboard'),
+
+    # url: '/customers/dashboard', name = core:customer_dashboard
+    path('customers/dashboard',
+         CustomerDashboardView.as_view(), name='customer_dashboard'),
 
     # url: '/dashboard', name = core:dashboard
     path('dashboard/', dashboard, name='dashboard'),
