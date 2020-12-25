dce58b3e3f109f6243f346501e5f76a86407962e
thehimel <himeldas66@gmail.com>
2020-12-25 21:28:31 +0100

    LawyerSignupForm updated

--- users/forms/lawyers.py
+++ users/forms/lawyers.py
@@ -1,10 +1,10 @@
 from allauth.account.forms import SignupForm
 from users.appvars import (
-    FIRST_NAME_MAX_LENGTH, LAST_NAME_MAX_LENGTH, MANAGER)
+    FIRST_NAME_MAX_LENGTH, LAST_NAME_MAX_LENGTH, LAWYER)
 from django import forms
 
 
-class ManagerSignupForm(SignupForm):
+class LawyerSignupForm(SignupForm):
 
     first_name = forms.CharField(
         max_length=FIRST_NAME_MAX_LENGTH, label='First Name')
@@ -12,9 +12,9 @@ class ManagerSignupForm(SignupForm):
         max_length=LAST_NAME_MAX_LENGTH, label='Last Name')
 
     def save(self, request):
-        user = super(ManagerSignupForm, self).save(request)
+        user = super(LawyerSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.user_type = MANAGER
+        user.user_type = LAWYER
         user.save()
         return user
